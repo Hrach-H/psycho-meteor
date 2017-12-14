18b2d4a5ec6bdcfc6cc29d53b4a94d02c30fca2b
hrachya.hovsepyan <hrach.hovsepyan@gmail.com>
2017-12-14 16:25:18 +0400

    Minor fix

--- imports/api/Questions.js
+++ imports/api/Questions.js
@@ -6,8 +6,14 @@ import { check } from 'meteor/check';
 export const Questions = new Mongo.Collection('questions');
 
 if (Meteor.isServer) {
-    Meteor.publish('questions', function publishQuestions(onlyTopics = false, topicId) {
+    Meteor.publish('questions', function publishQuestions(onlyTopics = false, topicId = false) {
         // If onlyTopics is true, we return only the 'topic' field of the collection and nothing else
-        return onlyTopics ? Questions.find({}, {fields: {"topic": 1}}) : Questions.find();
+        if (onlyTopics) {
+            return Questions.find({}, {fields: {"topic": 1}});
+        } else if (topicId) {
+            return Questions.find({topic: topicId});
+        } else {
+            return Questions.find();
+        }
     });
 }
